211d75b7ce6f40448ef9f1d4907ee39b90cf1b44
Tatyana <tanyarekhovskaya@yandex.ru>
2022-12-10 13:01:46 +0700

    Добавлены массивы для Пикселя и Байта

---
+++ Practikum.java
@@ -0,0 +1,6 @@
+public class Praktikum {
+   public static void main(String[] args) {
+        double[] feedExpensesCat = {100.50, 236.0, 510.6, 150.20, 80.0, 172.0, 135.4};
+        double[] feedExpensesHamster = {70.50, 146.0, 710.6, 250.20, 83.0, 19.0, 55.4};
+   }
+}
